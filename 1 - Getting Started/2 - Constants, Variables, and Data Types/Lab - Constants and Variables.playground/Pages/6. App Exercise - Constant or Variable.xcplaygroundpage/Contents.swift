ee08a4d2db705443181969c1b64cd2a7bf8b533a
Shabalin13 <d.shabalin@innopolis.university>
2022-01-21 16:36:14 +0300

    add Ex2

--- 1 - Getting Started/2 - Constants, Variables, and Data Types/Lab - Constants and Variables.playground/Pages/6. App Exercise - Constant or Variable.xcplaygroundpage/Contents.swift
+++ 1 - Getting Started/2 - Constants, Variables, and Data Types/Lab - Constants and Variables.playground/Pages/6. App Exercise - Constant or Variable.xcplaygroundpage/Contents.swift
@@ -11,7 +11,16 @@
 - Goal number of steps: The user's goal for number of steps to take each day
 - Average heart rate: The user's average heart rate over the last 24 hours
  */
-
+var name = "Dmitrii"
+print("A person can change his/her name and then it will have to be changed in the application")
+var age = 20
+print("Age increases every year")
+var numberOfStepsTakenToday = 9801
+print("The number of steps increases with each step taken and can be changed after the declaration")
+var goalNumberOfSteps = 12000
+print("The goal for the steps can also be changed every day")
+var averageHeartRate = 76
+print("Average heart rate can vary from day to day")
 
 /*:
  Now go back and add a line after each constant or variable declaration. On those lines, print a statement explaining why you chose to declare the piece of information as a constant or variable.
--- 1 - Getting Started/2 - Constants, Variables, and Data Types/Lab - Constants and Variables.playground/Pages/7. Exercise - Types and Type Safety.xcplaygroundpage/Contents.swift
+++ 1 - Getting Started/2 - Constants, Variables, and Data Types/Lab - Constants and Variables.playground/Pages/7. Exercise - Types and Type Safety.xcplaygroundpage/Contents.swift
@@ -3,17 +3,20 @@
  
  Declare two variables, one called `firstDecimal` and one called `secondDecimal`. Both should have decimal values. Look at both of their types by holding Option and clicking the variable name.
  */
-
-
+var firstDecimal = 13.4
+var secondDecimal = 19.9
 //:  Declare a variable called `trueOrFalse` and give it a boolean value. Try to assign it to `firstDecimal` like so: `firstDecimal = trueOrFalse`. Does it compile? Print a statement to the console explaining why not, and remove the line of code that will not compile.
-
-
+var trueOrFalse = true
+//firstDecimal = trueOrFalse
+print("One cannot be assigned to the other because they are of different types. Decimal is not the same as Bool")
 //:  Declare a variable and give it a string value. Then try to assign it to `firstDecimal`. Does it compile? Print a statement to the console explaining why not, and remove the line of code that will not compile.
-
-
+var name = "Dima"
+//firstDecimal = name
+print("The same problem. Decimal is not the same as String")
 //:  Finally, declare a variable with a whole number value. Then try to assign it to `firstDecimal`. Why won't this compile even though both variables are numbers? Print a statement to the console explaining why not, and remove the line of code that will not compile.
-
-
+var newNumber = 24
+//firstDecimal = newNumber
+print("The same problem. Decimal is not the same as Integer")
 /*:
 [Previous](@previous)  |  page 7 of 10  |  [Next: App Exercise - Tracking Different Types](@next)
  */
