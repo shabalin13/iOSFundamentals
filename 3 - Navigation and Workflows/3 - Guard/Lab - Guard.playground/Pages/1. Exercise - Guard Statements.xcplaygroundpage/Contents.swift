fe3ac9906a2003d8031781b134b24c59ce535e0c
Shabalin13 <d.shabalin@innopolis.university>
2022-02-07 16:48:17 +0300

    add Ex3

--- 3 - Navigation and Workflows/3 - Guard/Lab - Guard.playground/Pages/1. Exercise - Guard Statements.xcplaygroundpage/Contents.swift
+++ 3 - Navigation and Workflows/3 - Guard/Lab - Guard.playground/Pages/1. Exercise - Guard Statements.xcplaygroundpage/Contents.swift
@@ -4,11 +4,25 @@ import UIKit
 
  Imagine you want to write a function to calculate the area of a rectangle. However, if you pass a negative number into the function, you don't want it to calculate a negative area. Create a function called `calculateArea` that takes two `Double` parameters, `x` and `y`, and returns an optional `Double`. Write a guard statement at the beginning of the function that verifies each of the parameters is greater than zero and returns `nil` if not. When the guard has succeeded, calculate the area by multiplying `x` and `y` together, then return the area. Call the function once with positive numbers and once with at least one negative number.
  */
+func calculateArea(x: Double, y: Double) -> Double? {
+    guard x > 0 && y > 0 else {
+        return nil
+    }
+    return x * y
+}
 
-
+print(calculateArea(x: 4, y: 8))
+print(calculateArea(x: -3, y: 8))
 //:  Create a function called `add` that takes two optional integers as parameters and returns an optional integer. You should use one `guard` statement to unwrap both optional parameters, returning `nil` in the `guard` body if one or both of the parameters doesn't have a value. If both parameters can successfully be unwrapped, return their sum. Call the function once with non-`nil` numbers and once with at least one parameter being `nil`.
+func add(int1: Int?, int2: Int?) -> Int? {
+    guard let fst = int1, let scn = int2 else {
+        return nil
+    }
+    return fst + scn
+}
 
-
+print(add(int1: 3, int2: 6))
+print(add(int1: nil, int2: 6))
 /*:
  When working with UIKit objects, you will occasionally need to unwrap optionals to handle user input. For example, the text fields initialized below have `text` properties that are of type `String?`.
 
@@ -28,11 +42,16 @@ firstNameTextField.text = "Jonathan"
 lastNameTextField.text = "Sanders"
 ageTextField.text = "28"
 
-
-
+func createUser() -> User? {
+    guard let firstName = firstNameTextField.text, let lastName = lastNameTextField.text, let age = ageTextField.text else {
+              return nil
+          }
+    return User(firstName: firstName, lastName: lastName, age: age)
+}
 //:  Call the function you made above and capture the return value. Unwrap the `User` with standard optional binding and print a statement using each of its properties.
-
-
+if let user = createUser() {
+    print("firstName: \(user.firstName)\tlastName: \(user.lastName)\tage: \(user.age)")
+}
 /*:
 page 1 of 2  |  [Next: App Exercise - Guard](@next)
  */
